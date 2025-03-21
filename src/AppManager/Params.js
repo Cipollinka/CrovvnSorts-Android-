11720ba3b1c461c9ca968c1b28783998ce4d7102
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-03-21 12:03:50 +0200

    add AppManager

---
+++ src/AppManager/Params.js
@@ -0,0 +1,6 @@
+export default class Params {
+  static keyPush = '66fe0db6-080b-4462-b47e-af20e642e465'; // ключ OneSignal
+  static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
+  static targetDate = '2025-03-24'; // дата закінчення відльожки
+  static bodyLin = 'https://magical-august-brilliance.space/1HmNx8qt'; // тіло лінки з якою будемо працювати
+}
